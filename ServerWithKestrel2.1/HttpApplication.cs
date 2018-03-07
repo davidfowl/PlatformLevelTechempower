dfaa77a94bb5f8c5c78c76c53f5c0ac1c21df6c3
David Fowler <davidfowl@gmail.com>
2018-03-07 00:10:56 -0800

    Clean up connections and echos

---
+++ ServerWithKestrel2.1/HttpApplication.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Buffers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Protocols;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using HttpVersion = Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http.HttpVersion;
+
+namespace ServerWithKestrel21
+{
+    public static class HttpApplicationConnectionBuilderExtensions
+    {
+        public static IConnectionBuilder UseHttpApplication<TConnection>(this IConnectionBuilder builder) where TConnection : HttpConnection, new()
+        {
+            return builder.Use(next => new HttpApplication<TConnection>().ExecuteAsync);
+        }
+    }
+
+    public class HttpApplication<TConnection> where TConnection : HttpConnection, new()
+    {
+        public Task ExecuteAsync(ConnectionContext connection)
+        {
+            var parser = new HttpParser<HttpConnection>();
+
+            var httpConnection = new TConnection
+            {
+                Connection = connection,
+                Parser = parser
+            };
+            return httpConnection.ExecuteAsync();
+        }
+    }
+
+    public class HttpConnection : IHttpHeadersHandler, IHttpRequestLineHandler
+    {
+        private State _state;
+
+        public ConnectionContext Connection { get; set; }
+
+        internal HttpParser<HttpConnection> Parser { get; set; }
+
+        public virtual void OnHeader(Span<byte> name, Span<byte> value)
+        {
+
+        }
+
+        public virtual void OnStartLine(HttpMethod method, HttpVersion version, Span<byte> target, Span<byte> path, Span<byte> query, Span<byte> customMethod, bool pathEncoded)
+        {
+
+        }
+
+        public virtual Task ProcessRequestAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            while (true)
+            {
+                try
+                {
+                    while (true)
+                    {
+                        var result = await Connection.Transport.Input.ReadAsync();
+                        var buffer = result.Buffer;
+                        var consumed = buffer.Start;
+                        var examined = buffer.End;
+
+                        try
+                        {
+                            if (!buffer.IsEmpty)
+                            {
+                                ParseHttpRequest(buffer, out consumed, out examined);
+
+                                if (_state != State.Body && result.IsCompleted)
+                                {
+                                    // Bad request
+                                    break;
+                                }
+
+                                if (_state == State.Body)
+                                {
+                                    await ProcessRequestAsync();
+
+                                    _state = State.StartLine;
+                                }
+                            }
+                            else if (result.IsCompleted)
+                            {
+                                break;
+                            }
+                        }
+                        finally
+                        {
+                            Connection.Transport.Input.AdvanceTo(consumed, examined);
+                        }
+                    }
+
+                    Connection.Transport.Input.Complete();
+                }
+                catch (Exception ex)
+                {
+                    Connection.Transport.Input.Complete(ex);
+                }
+                finally
+                {
+                    Connection.Transport.Output.Complete();
+                }
+            }
+        }
+
+        private void ParseHttpRequest(ReadOnlySequence<byte> buffer, out SequencePosition consumed, out SequencePosition examined)
+        {
+            consumed = buffer.Start;
+            examined = buffer.End;
+
+            if (_state == State.StartLine)
+            {
+                if (Parser.ParseRequestLine(this, buffer, out consumed, out examined))
+                {
+                    _state = State.Headers;
+                    buffer = buffer.Slice(consumed);
+                }
+            }
+
+            if (_state == State.Headers)
+            {
+                if (Parser.ParseHeaders(this, buffer, out consumed, out examined, out int consumedBytes))
+                {
+                    _state = State.Body;
+                }
+            }
+        }
+
+        private enum State
+        {
+            StartLine,
+            Headers,
+            Body
+        }
+    }
+}
--- ServerWithKestrel2.1/Program.cs
+++ ServerWithKestrel2.1/Program.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Protocols;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace ServerWithKestrel21
 {
@@ -17,24 +14,19 @@ namespace ServerWithKestrel21
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseKestrel(o =>
+                .UseKestrel(options =>
                 {
-                    // This end point isn't using ASP.NET Core but a custom HttpServer
-                    // built on top of Kestrel's transport layer
-                    o.Listen(IPAddress.Loopback, 8080, builder =>
+                    options.Listen(IPAddress.Any, 6005, builder =>
                     {
-                        builder.Use(HttpServer);
+                        builder.UseEchoServer();
+                    });
+
+                    options.Listen(IPAddress.Loopback, 8080, builder =>
+                    {
+                        builder.UseHttpApplication<PlainTextConnection>();
                     });
                 })
                 .UseStartup<Startup>()
                 .Build();
-
-        public static ConnectionDelegate HttpServer(ConnectionDelegate next)
-        {
-            // We're ignoring next because we don't want to call into the default
-            // HttpServer implementation
-            var application = new PlainTextRawApplication();
-            return application.ExecuteAsync;
-        }
     }
 }
